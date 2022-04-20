aa1d235b4a574b30aad76b88d11bc9310d933dfe
Nicholas Wachira <100451980+Tocopro@users.noreply.github.com>
2022-04-20 09:47:30 -0500

    Create squaring_numbers.py

---
+++ squaring_numbers.py
@@ -0,0 +1,12 @@
+
+
+# list elements into squares
+def squaring():
+    squared_numbers = []
+    numbers = [1, 2, 3, 4, 5, 6]
+    for i in numbers:
+        squared_numbers.append(i ** 2)
+    print(squared_numbers)
+
+
+squaring()
